0f5e910f961198f4489f2557b3bb32fb163432a9
rulli-aji_gunawan <rully.okayama@gmail.com>
2022-07-02 00:12:07 +0700

    Recyclerview APP Add profile

--- app/src/main/java/com/rulliajigunawan/recyclerview/MainActivity.kt
+++ app/src/main/java/com/rulliajigunawan/recyclerview/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.rulliajigunawan.recyclerview
 
 import android.app.Dialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.Window
 import android.widget.Button
 import android.widget.ImageView
@@ -10,6 +13,7 @@ import android.widget.TextView
 import com.rulliajigunawan.recyclerview.adapter.AdapterTeamBola
 import com.rulliajigunawan.recyclerview.databinding.ActivityMainBinding
 import com.rulliajigunawan.recyclerview.model.Pemain
+import org.intellij.lang.annotations.JdkConstants
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -54,6 +58,25 @@ class MainActivity : AppCompatActivity() {
                 }.show()
             }
         })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        setMode(item.itemId)
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun setMode(selectionMode : Int){
+        when (selectionMode) {
+            R.id.myprofile -> {
+                val intent = Intent(this,Profile::class.java)
+                startActivity(intent)
+            }
+        }
     }
 
 
